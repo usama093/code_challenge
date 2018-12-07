0e4f6dada7cab61d12ecda341e8ea051f05c04a9
Usama Afzal <usama@Apples-MacBook-Pro-3.local>
2018-12-07 17:16:07 +0100

    Initail styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: this.props.message };
+  }
+
+  resetComm() {
+    this.setState({ message: "" });
+  }
+
+  updateComm(message) {
+    this.setState({
+      message
+    });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    const { message } = this.state;
+    let messageView;
+    if (message !== "") {
+      messageView = (
+        <div>
+          <div class="form-group">
+            <p>
+              <b>Scotty : </b>
+              {message}
+            </p>
+          </div>
+
+          <button
+            type="submit"
+            class="btn btn-warning btn-lg float-right"
+            onClick={() => this.resetComm()}
           >
-            Learn React
-          </a>
-        </header>
+            Clear <i class="fa fa-trash" aria-hidden="true" />
+          </button>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <div class="container py-5">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="row">
+                <div class="col-md-6 mx-auto">
+                  <div class="card rounded-0">
+                    <div class="card-header">
+                      <h3 class="mb-0">Style me, Redux me, Surprise me. ;)</h3>
+                    </div>
+                    <div class="card-body">
+                      <div class="form-group">
+                        <input
+                          type="text"
+                          class="form-control form-control-lg rounded-0"
+                          placeholder="Say something... "
+                          onChange={event =>
+                            this.updateComm(event.target.value)
+                          }
+                          value={message}
+                        />
+                      </div>
+                      {messageView}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
